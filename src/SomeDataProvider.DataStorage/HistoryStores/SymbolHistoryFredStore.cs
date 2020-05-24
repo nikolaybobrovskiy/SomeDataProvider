60b387e8588bf19c4b7fb3f725fc7656e0bb1d52
Nikolay Bobrovskiy <nikolay.bobrovskiy@gmail.com>
2020-05-25 00:28:46 +0300

    SymbolHistoryFredStore

--- src/SomeDataProvider.DataStorage/HistoryStores/SymbolHistoryFredStore.cs
+++ src/SomeDataProvider.DataStorage/HistoryStores/SymbolHistoryFredStore.cs
@@ -1,10 +1,14 @@
 namespace SomeDataProvider.DataStorage.HistoryStores
 {
 	using System;
+	using System.Linq;
 	using System.Threading;
 	using System.Threading.Tasks;
 
+	using NBLib.BuiltInTypes;
+
 	using SomeDataProvider.DataStorage.Definitions;
+	using SomeDataProvider.DataStorage.InMem;
 
 	public class SymbolHistoryFredStore : ISymbolHistoryStore, IDisposable
 	{
@@ -22,9 +26,22 @@ namespace SomeDataProvider.DataStorage.HistoryStores
 			_fredService = fredService;
 		}
 
-		public Task<GetSymbolHistoryResult> GetSymbolHistoryAsync(ISymbol symbol, HistoryInterval historyInterval, DateTime start, DateTime end, int limit, string? continuationToken, CancellationToken cancellationToken = default)
+		public async Task<GetSymbolHistoryResult> GetSymbolHistoryAsync(ISymbol symbol, HistoryInterval historyInterval, DateTime start, DateTime end, int limit, string? continuationToken, CancellationToken cancellationToken = default)
 		{
-			throw new NotImplementedException();
+			var fredSymbol = symbol.Code.GetFredSymbol();
+
+			var offset = 0;
+			if (!continuationToken.IsEmpty())
+				offset = int.Parse(continuationToken);
+
+			var observationsData = await _fredService.GetObservationsAsync(fredSymbol.SeriesId, start: start, end: end, transformation: fredSymbol.Units, offset: offset, limit: limit, cancellationToken: cancellationToken);
+			if (observationsData == null) return GetSymbolHistoryResult.Empty;
+
+			return new GetSymbolHistoryResult(observationsData.Observations.Where(x => x.Value != null).Select(x =>
+			{
+				var v = x.Value!.Value;
+				return new SymbolHistoryRecord(x.Date, v, v, v, v, 0);
+			}).ToArray(), observationsData.Observations.Length < limit ? null : (offset + limit).ToString());
 		}
 
 		public void Dispose()
